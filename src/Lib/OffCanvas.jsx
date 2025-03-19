f9c9c9320c63e34e9b8b491146a927c9208beb14
olorunwa-web <olorunwamoliki@gmail.com>
2025-03-19 19:35:27 +0100

    first commit

--- src/Lib/OffCanvas.jsx
+++ src/Lib/OffCanvas.jsx
@@ -6,6 +6,7 @@ import menuicon from '../assets/menu_33dp_1F253F_FILL0_wght400_GRAD0_opsz40.svg'
 import ventimg from '../assets/Logo.svg';
 import bluearrow from '../assets/Vector.svg';
 import whitearrow from '../assets/Vector (1).svg';
+import { Link } from 'react-scroll'
 
 
 const OffCanvas = ({ name, ...props }) => {
@@ -33,10 +34,10 @@ const OffCanvas = ({ name, ...props }) => {
                      <Offcanvas.Body>
                          <section className = 'd-flex flex-column gap-4'>
                             <div className = 'd-flex flex-column  nav-homes pt-2'>
-                               <p>Service</p>
-                               <p>About us</p>
-                               <p>Benefits</p>
-                               <p>FAQ ?</p>
+                               <p><Link to = "service" smooth = {true} duration = {500} >Service</Link></p>
+                               <p><Link to = "about" smooth = {true} duration = {500} >About us</Link></p>
+                               <p><Link to = "benefit" smooth = {true} duration = {500} >Benefit</Link></p>
+                               <p><Link to = "faq" smooth = {true} duration = {500} >FAQ ? </Link></p>
                             </div>
                             <div className = 'd-flex flex-column gap-3'>
                                <div className = 'd-flex gap-2 align-items-center book-div-1 ps-4'>
